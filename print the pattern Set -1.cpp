00df2987be0634aa42c1f088edec228bb347c712
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2020-07-22 13:51:33 +0530

    Update print the pattern Set -1.cpp

--- print the pattern Set -1.cpp
+++ print the pattern Set -1.cpp
@@ -1,7 +1,10 @@
-for(int i=0;i<n;i++){ for(int j=n;j>0;j--){
-int k=n-i;
-while(k--)
-cout << j << " ";
-}
-cout << '$';
+for(int i=0;i<n;i++)
+{ 
+  for(int j=n;j>0;j--)
+  {
+    int k=n-i;
+    while(k--)
+      cout << j << " ";
+  }
+  cout << '$';
 }
