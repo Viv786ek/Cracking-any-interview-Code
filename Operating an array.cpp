f108ecadb6be164cf0e52ae4052b381057962353
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2021-01-21 17:53:42 +0530

    Create Operating an array.cpp

---
+++ Operating an array.cpp
@@ -0,0 +1,21 @@
+bool searchEle(int a[],int x)
+{
+    int i = 0;
+    while(a[i] != -1)
+	if(a[i++] == x) return true;
+	return false;
+}
+bool insertEle(int a[],int y,int yi)
+{
+    int i = 0;
+    while(a[i] != -1)
+    if(i++ == yi){ a[i-1] = y; return true;}
+    return false;
+}
+bool deleteEle(int a[],int z)
+{
+    int i = 0;
+    while(a[i] != -1)
+	if(a[i++] == z){a[i-1] = -1;}
+	return true;
+}
