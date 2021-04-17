563e128cbe381253eefdbc123543c3bf437e4382
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2021-04-17 16:55:34 +0530

    Update Upper case conversion .cpp

--- Upper case conversion .cpp
+++ Upper case conversion .cpp
@@ -1,5 +1,19 @@
 string transform(string s)
 {
+for(int i = 0;i < s.length();i++){
+if(s[i] == ' '){
+s[i+1] = s[i+1] -'a' + 'A';
+}
+}
+s[0] = s[0] - 'a' +'A';
+return s;
+}
+
+
+
+
+string transform(string s)
+{
     // code here
     s[0] = int(s[0]) - 32;
     for(int i=1;i<s.size();i++){
