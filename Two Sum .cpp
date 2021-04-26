91f30e7d0f614d7b0fcad60c71acced7da425a71
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2021-04-27 00:03:25 +0530

    Update Two Sum .cpp

--- Two Sum .cpp
+++ Two Sum .cpp
@@ -1,3 +1,24 @@
+void printPairs(int arr[], int arr_size, int sum)
+{
+    unordered_set<int> s;
+    for (int i = 0; i < arr_size; i++) 
+    {
+        int temp = sum - arr[i];
+
+        if (s.find(temp) != s.end())
+            cout << "Pair with given sum "
+                 << sum << " is (" << arr[i] << "," 
+                    << temp << ")" << endl;
+
+        s.insert(arr[i]);
+    }
+}
+
+
+
+
+
+//2nd
 class Solution
 {
     public:
