e4914df3129d540ca6b3999681ed11f47f0fcce5
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2021-04-26 23:58:35 +0530

    Create Two Sum .cpp

---
+++ Two Sum .cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    bool keypair(vector<int> A, int N, int X)
+    {
+        // Your code goes here
+        sort(A.begin(),A.end());
+        int l=0,j=N-1;
+        int f=0;
+        while(l<j){
+            if(A[l]+A[j] == X){
+                f=1;
+                break;
+        }
+            else if(A[l]+A[j] >X){
+                j--;
+            }
+            else{
+                l++;
+            }
+        }
+        if(f==0)
+            return false;
+        return true;
+    }
+};
